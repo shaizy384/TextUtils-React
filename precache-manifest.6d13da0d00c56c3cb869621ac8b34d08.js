e7c9bf7e40b02849fbb1dcd21e256eae9f9d671e
shaizy384 <122147898+shaizy384@users.noreply.github.com>
2023-01-08 12:29:09 +0500

    Updates

---
+++ precache-manifest.6d13da0d00c56c3cb869621ac8b34d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5e92a8b68ad03b65ef5",
+    "url": "/TextUtils-React/static/css/main.9ca42434.chunk.css"
+  },
+  {
+    "revision": "d5e92a8b68ad03b65ef5",
+    "url": "/TextUtils-React/static/js/main.d5e92a8b.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2591e466b5f270c0c477",
+    "url": "/TextUtils-React/static/js/runtime~main.2591e466.js"
+  },
+  {
+    "revision": "64812803849225ebd46b0d189bfadfe7",
+    "url": "/TextUtils-React/index.html"
+  }
+];
