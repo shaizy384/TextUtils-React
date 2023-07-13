18ad5d7c20ecd2e5fdd81b3ae07af555ab41ef73
Shahzaib Qasim <shahzaibqasim384@gmail.com>
2023-07-13 19:37:41 +0500

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-//import About from './components/About';
+import About from './components/About';
 import { useState } from 'react';
 import Alert from './components/Alert';
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route
-// } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type) =>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
@@ -24,9 +24,9 @@ function App() {
     setTimeout(() => {
       setAlert(null)
     }, 1500);
-    }
+  }
 
-  const toggleMode = () =>{
+  const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
@@ -40,19 +40,19 @@ function App() {
   }
   return (
     <div>
-      {/* <BrowserRouter> */}
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-        <Alert alert={alert}/>
+      <BrowserRouter>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
         <div className="container my-3">
-          {/* <Routes>
-              <Route path="/"
-                element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below"  mode={mode}/>}
-              />
-              <Route path="/about" element={<About mode={mode}/>}/>
-          </Routes> */}
-          <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Charater Counter"  mode={mode}/>
+          <Routes>
+            <Route path="/"
+              element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Charater Counter" mode={mode} />}
+            />
+            <Route path="/about" element={<About mode={mode} />} />
+          </Routes>
+          {/* <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Charater Counter"  mode={mode}/> */}
         </div>
-      {/* </BrowserRouter> */}
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-//import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">{props.title}</a>
+          <NavLink className="navbar-brand" href="/">{props.title}</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="/">Home</a>
-                {/* <Link className="nav-link" aria-current="page" to="/">Home</Link> */}
+                {/* <a className="nav-link" aria-current="page" href="/">Home</a> */}
+                <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">About</a>
-                {/* <Link className="nav-link" to="/about">About</Link> */}
+                {/* <a className="nav-link" href="/about">About</a> */}
+                <NavLink className="nav-link" to="/about">About</NavLink>
               </li>
             </ul>
-            <div className={`form-check form-switch text-${props.mode === "light"?"dark":"light"} `}>
-              <input className="form-check-input" onClick={props.toggleMode} type="checkbox"  id="flexSwitchCheckDefault"/>
+            <div className={`form-check form-switch text-${props.mode === "light" ? "dark" : "light"} `}>
+              <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
               <label className="form-check-label" htmfor="flexSwitchCheckDefault">Enable DarkMode</label>
             </div>
           </div>
